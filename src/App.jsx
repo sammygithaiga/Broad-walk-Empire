c1f636e4bbd0030eaddea1845a2b364a83cc6fd8
samuel mwangi githaiga <samuelgithaiga913@gmail.com>
2024-05-07 22:30:12 +0300

    app

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,54 @@
-import React from 'react';
-// import { Route, Switch } from 'react-router-dom';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import Filter from './components/Filter';
-import Footer from './components/Footer';
-import Header from './components/Header';
-import SearchBar from './components/SearchBar';
-import LogIn from './components/LogIn';
-import Navbar from './components/NavBar';
 
-const App = () => {
+
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+import Filter from "./components/Filter";
+import Footer from "./components/Footer";
+import Header from "./components/Header";
+import SearchBar from "./components/SearchBar";
+import LogIn from "./components/LogIn";
+import Navbar from "./components/NavBar";
+
+export default function App() {
   return (
-    <div>
-      <Header />
-      <Navbar />
-      <Router/>
-      <Switch>
-        <Route exact path="/" component={SearchBar} />
-        <Route path="/login" component={LogIn} />
-      </Switch>
-      <Router/>
-      <Filter />
-      <Footer />
-    </div>
-  );
-};
+    <Router>
+      <div>
+        <Header />
+        <LogIn/>
+        <Navbar />
 
-export default App;
+        <Switch>
+          <Route path="/about">
+            <About />
+          </Route>
+          <Route path="/users">
+            <Users />
+          </Route>
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
+
+        <Filter />
+        <Footer />
+      </div>
+    </Router>
+  );
+}
+
+function Home() {
+  return <SearchBar />;
+}
+
+function About() {
+  return <h2>About</h2>;
+}
+
+function Users() {
+  return <h2>Users</h2>;
+}
