c6e4df4b7ae34b7c0eb8bf437e4989936cb72f91
sammygithaiga <162136347+sammygithaiga@users.noreply.github.com>
2024-05-08 09:45:16 +0300

    Update Users.jsx

--- src/components/Users.jsx
+++ src/components/Users.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 
-function LogIn() {
+function Users() {
   return (
     <div>
       
@@ -8,4 +8,4 @@ function LogIn() {
   )
 }
 
-export default LogIn;
+export default Users;
