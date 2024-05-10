955030dda515afecd266278ae1f367ed8135ddb5
lewis karanu <lewiskaranu0@gmail.com>
2024-05-10 09:58:11 +0300

    last

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,19 +1,16 @@
-
-
-
 function Header() {
   return (
     <div>
       <>
-      <div className="flex justify-center p-5">
-        <h1 className="font-bold text-5xl"> Broad-walk-Empire Real Estate</h1>
-      </div>
-    </>
+        <div className="flex justify-center p-5">
+          <h1 className="font-bold text-5xl">
+            {" "}
+            Welcome to Broad-walk-Empire Real Estate
+          </h1>
+        </div>
+      </>
     </div>
-  )
+  );
 }
 
-export default Header
-
-
-
+export default Header;
