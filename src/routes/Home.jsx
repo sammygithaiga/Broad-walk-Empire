a461cad9ee639f1b244262d8cd182ae03a84d67a
sammygithaiga <162136347+sammygithaiga@users.noreply.github.com>
2024-05-10 12:19:31 +0300

    Update Home.jsx

--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -1,39 +1,18 @@
-import { useEffect, useState } from 'react';
-import Footer from '../components/Footer';
+x
 import NavBar from '../components/NavBar';
-
-import SearchBar from '../components/SearchButton';
 
 
 
 function Home() {
-  const [properties, setProperties] = useState([]);
-  const [searchInput, setSearchInput] = useState('');
-
-  useEffect(() => {
-    fetch('http://localhost:3000/properties')
-      .then((res) => res.json())
-      .then((data) => setProperties(data))
-      .catch((error) => console.error('Error fetching data:', error));
-  }, []);
-
- 
-  const handleSearchInputChange = (event) => {
-    setSearchInput(event.target.value);
-  };
-
- 
- const filteredProperties = properties.filter((property) =>
-    property.name)
-  ;
-  return (
+	 return (
 	    <NavBar/>
-	  <main>
-	   <SearchBar />
-	    <Footer/>
-	  </main>
+	
 	
   )
 }
 
 export default Home;
+
+
+
+
