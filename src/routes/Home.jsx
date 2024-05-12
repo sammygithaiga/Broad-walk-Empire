bded4eb2a47cef0e14f5eab2a1fcdc99933f202d
IVON1010 <162134968+IVON1010@users.noreply.github.com>
2024-05-12 20:56:38 +0300

    Update Home.jsx

--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -1,11 +1,13 @@
-x
 import NavBar from '../components/NavBar';
-
+import SearchButton from '..components/SearchButton';
+import SearchProrety from '..components/SearchProrety';
 
 
 function Home() {
 	 return (
 	    <NavBar/>
+		 <SearchButton/>
+		 <SearchProrety/>
 	
 	
   )
