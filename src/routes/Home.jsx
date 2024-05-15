d16d9f5d9316b3f55c7e2190228265b4e394778d
IVON1010 <162134968+IVON1010@users.noreply.github.com>
2024-05-15 10:35:32 +0300

    Update Home.jsx

--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -2,8 +2,25 @@ import Filter from '../components/Filter';
 import PropertyDetails from './PropertyDetails';
 
 
+const apiUrl = 'https://my-json-server.typicode.com/sammygithaiga/Broad-walk-Empire/property';
 
 function Home() {
+
+fetch(apiUrl)
+  .then(response => {
+    if (!response.ok) {
+      throw new Error('Network response was not ok');
+    }
+    return response.json();
+  })
+  .then(property => {
+    // Process the retrieved user data
+    console.log(property);
+  })
+  .catch(error => {
+    console.error('Error:', error);
+  });
+
 	 return (
 		<>
 		<Filter />
@@ -15,6 +32,7 @@ function Home() {
 }
 
 export default Home;
+
 
 
 
